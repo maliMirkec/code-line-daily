069a300935e34c66670d1c7943863500257b9936
Star Bist <malimirkeccita@gmail.com>
2020-02-19 16:20:22 +0100

    Updating gulp files.

--- gulpfile.js/clean.js
+++ gulpfile.js/clean.js
@@ -1,19 +1,19 @@
 // const gulp = require('gulp')
-const del = require('del')
+const del = require('del');
 
-const { helpers } = require('./helpers')
+const { helpers } = require('./helpers');
 
 // Will delete dist folder
-function cleanStart () {
-  return del(helpers.dist())
+function cleanStart() {
+  return del(helpers.dist());
 }
 
 // Will delete dist folder
-function cleanLines () {
-  return del(helpers.trim(`${helpers.dist()}/${global.config.html.dist}/line/`))
+function cleanLines() {
+  return del(helpers.trim(`${helpers.dist()}/${global.config.html.dist}/line/`));
 }
 
 exports.clean = {
   cleanStart,
-  cleanLines
-}
+  cleanLines,
+};
